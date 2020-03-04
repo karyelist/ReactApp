b6f0ff3426b37b5b5fc6429832e0e83822b1d5cf
karyelist <mkarakus2013@gmail.com>
2020-03-04 13:55:54 +0300

    React Component-Props

---
+++ src/components/User.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class User extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                <li>İsim: {this.props.name}</li>
+                <li>Departman : {this.props.department}</li>
+                <li>Maaş : {this.props.salary} </li></ul>
+            </div>
+        )
+    }
+}
+
+export default User;
